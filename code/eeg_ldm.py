488d027244c9545a977aec81eca292d4be85af2b
krishkrosh <krish.life21@tmail.com>
2024-01-17 10:38:19 -1000

    add config_mbm to eeg_ldm

--- code/eeg_ldm.py
+++ code/eeg_ldm.py
@@ -13,6 +13,7 @@ import copy
 
 # own code
 from config import Config_Generative_Model
+from config import Config_MBM_EEG
 from dataset import  create_EEG_dataset
 from dc_ldm.ldm_for_eeg import eLDM
 from eval_metrics import get_similarity_metric
