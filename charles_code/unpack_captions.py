a3f01e5c3995978b0dd2a976d65116d0e640557d
Charles Liu <charles.liu0309@gmail.com>
2024-01-19 03:01:44 +0000

    added correct captions script

--- charles_code/unpack_captions.py
+++ charles_code/unpack_captions.py
@@ -5,9 +5,8 @@ with open('datasets/captions.json', 'r') as file:
     captions_data = json.load(file)
 
 # Create a .txt file for each caption
-for item in captions_data:
-    nsd_id = int(item['nsdId']) + 1  # Increment nsdId by 1
-    filename = f'datasets/coco_labels/{nsd_id}.txt'
+for i, item in enumerate(captions_data):
+    filename = f'datasets/coco_labels/{i+1}.txt'
 
     with open(filename, 'w') as txt_file:
         txt_file.write(item['caption'])
